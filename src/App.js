df5f6fe6eba050b54283cf15a92601ad4c102664
George Betts <george@providefinance.com>
2024-08-01 11:45:44 +0100

    Ran create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Provide Finance Interview Test</h1>
+      <p>Please check the Readme for details about the tests.</p>
+    </div>
+  );
+}
+
+export default App;
